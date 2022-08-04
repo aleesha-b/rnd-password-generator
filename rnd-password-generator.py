d5048c589908df2a9bb98e29c781589862668b13
aleesha-b <aleeshabretnall@gmail.com>
2022-08-04 09:51:26 +0800

    user chooses length of password

--- rnd-password-generator.py
+++ rnd-password-generator.py
@@ -1,8 +1,18 @@
 import random
 
 password = ""
-for i in range(10):
-    random_integer = random.randint(0, 126)
+while True:
+    password_length = input("Please enter the desired length of your password (between 1 and 50 characters): ")
+    try:
+        password_length = int(password_length)
+        if password_length in range(51):
+            break
+        else:
+            print("Please enter a number between 1 and 50")
+    except ValueError:
+        print("You did not enter a valid number")
+for i in range(password_length):
+    random_integer = random.randint(48, 122)
     password += chr(random_integer)
 
 print(password, len(password))
